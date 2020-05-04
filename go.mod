32a2a962e72a5b678dadf9cbec31e20280a5d0b5
Nate Robinson <Nate@flashframe.io>
2020-05-04 10:38:37 -0500

    added modules

---
+++ go.mod
@@ -0,0 +1,11 @@
+module cannonballrun
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
+	github.com/jinzhu/gorm v1.9.12
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/strava/go.strava v0.0.0-20180612235916-99ebe972ba16
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+)
---
+++ middleware.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// JwtVerify is a middlware function to parse x-access-token
+func JwtVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		var header = r.Header.Get("x-access-token") //Grab the token from the header
+
+		header = strings.TrimSpace(header)
+
+		if header == "" {
+			//Token is missing, returns with error code 403 Unauthorized
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(Exception{Message: "Missing auth token"})
+			return
+		}
+		tk := Token{}
+		claims := jwt.MapClaims{}
+		_, err := jwt.ParseWithClaims(header, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret"), nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(Exception{Message: err.Error()})
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", tk)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// CommonMiddleware --Set content-type
+func CommonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+		next.ServeHTTP(w, r)
+	})
+}
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
